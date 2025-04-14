79351e8441079be8548ea16c57dc683393673f3e
BlackJenn <jennifer@gmail.com>
2025-04-13 22:32:42 -0500

    agregamos funcion principal

---
+++ Semana2.py
@@ -0,0 +1,10 @@
+from categorias import categorizar_edad
+
+edad = int(input("Ingresa tu edad: "))
+
+if edad.lstrip("-").isdigit():
+    edad = int(edad)
+    categoria = categorizar_edad(edad)
+    print (f"Segun tu edad eres un {categoria}")
+else:
+    print("ingresaste un valor invalido")
